62c628a23e3b3c65a98591c247674da8f0c05a6d
eloiantonio <eloiat3@gmail.com>
2021-08-09 16:23:33 -0300

    Primeira tentativa de entender a tarefa

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,16 @@
 import pickle
 from fastapi import FastAPI
+from sklearn import *
 
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+def titanic(Sex:int, Age:float, Lifeboat:int, Pclass: int):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+    X = [Sex, Age, Lifeboat, Pclass]
+    result = titanic.predict(X)
 
 @app.get('/model')
 def get():
-    return {'hello':'test'}
+    return {'survived':result}
