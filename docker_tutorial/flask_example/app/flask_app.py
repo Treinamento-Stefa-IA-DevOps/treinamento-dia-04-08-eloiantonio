12ca7d5dd62d801d41c9bc63afd2f887a7adf9b5
eloiantonio <eloiat3@gmail.com>
2021-08-09 15:26:11 -0300

    tentativa de ajuste do docker

--- docker_tutorial/flask_example/app/flask_app.py
+++ docker_tutorial/flask_example/app/flask_app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    with psycopg2.connect(f "dbname={os.getenv("DB_NAME")} user={"DB_USER"} password:{os.getenv:"DB_PASS"}") as conn:
+    with psycopg2.connect(f"dbname={os.getenv("DB_NAME")} user={"DB_USER"} password:{os.getenv:"DB_PASS"}") as conn:
         cur = conn.cursor()
         cur.execute("SELECT * FROM test;")
         rows = cur.fetchall()  # retorna o resultado como tuplas, onde cada item dentro da tupla [e o valor de uma coluna]
